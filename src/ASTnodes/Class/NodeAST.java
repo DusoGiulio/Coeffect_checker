bbf7c5e0f0951cf87f1ad5e6ebddca382fc0f404
DusoGiulio <127857942+DusoGiulio@users.noreply.github.com>
2024-03-08 11:29:52 +0100

    Delete src directory

--- src/ASTnodes/Class/NodeAST.java
+++
@@ -1,7 +0,0 @@
-package ASTnodes.Class;
-
-/**
- * Classe astratta di base per i nodi dell'AST (Abstract Syntax Tree).
- * Tutti i nodi dell'AST devono estendere questa classe.
- */
-public  abstract class NodeAST {}
--- src/parser/ASTnodes/Class/NodeAST.java
+++
@@ -1,7 +0,0 @@
-package parser.ASTnodes.Class;
-
-/**
- * Classe astratta di base per i nodi dell'AST (Abstract Syntax Tree).
- * Tutti i nodi dell'AST devono estendere questa classe.
- */
-public  abstract class NodeAST {}
